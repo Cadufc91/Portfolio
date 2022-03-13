c57351302adc6ff4fd9eef0d04cebd6141a8fce0
Cadufc91 <fernandes.cadu@gmail.com>
2022-03-13 14:29:21 -0300

    Slider adicionado

--- js/main.js
+++ js/main.js
@@ -15,3 +15,19 @@ for (const link of links){
     })
 }
 
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
